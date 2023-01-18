3240c2d626ca6b29e37f07028a55734635e60890
DamienDeSousa <desousadamien30@gmail.com>
2023-01-18 08:16:52 +0100

    # header banner style
     * use BEM notation for headerbanner
     * use sass for headerbanner

--- components/HeaderBanner/index.tsx
+++ components/HeaderBanner/index.tsx
@@ -1,15 +1,9 @@
-import styles from './index.module.css'
+import styles from '../../styles/HeaderBanner.module.scss'
 
 export default function HeaderBanner() {
   return (
-    <div
-      style={{
-        display: 'flex',
-        flexDirection: 'row',
-        justifyContent: 'center',
-      }}
-    >
-      <h1 className={styles.headerbanner_text}>RACHELGUITTON</h1>
+    <div className={styles.headerbanner}>
+      <h1>RACHELGUITTON</h1>
     </div>
   )
 }
